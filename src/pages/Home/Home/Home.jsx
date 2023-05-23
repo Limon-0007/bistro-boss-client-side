f47fefc13c79abb50834f130bcbec305bc4e9fdc
Limon-0007 <limonahmed3023@gmail.com>
2023-05-24 02:16:55 +0600

    added react helmet to change title dynamically

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import ChefRecommends from "../ChefRecommands/ChefRecommends";
@@ -6,16 +7,19 @@ import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Category></Category>
-            <PopularMenu></PopularMenu>
-            <ChefRecommends></ChefRecommends>
-            <Featured></Featured>
-            <Testimonials></Testimonials>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <Category></Category>
+      <PopularMenu></PopularMenu>
+      <ChefRecommends></ChefRecommends>
+      <Featured></Featured>
+      <Testimonials></Testimonials>
+    </div>
+  );
 };
 
 export default Home;
