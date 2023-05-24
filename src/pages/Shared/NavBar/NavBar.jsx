0684e4653923972bdebd94de4534d47567b4927a
Limon-0007 <limonahmed3023@gmail.com>
2023-05-24 10:04:24 +0600

    use custom hooks

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from "../../../assets/home/Group 1.png"
 
 const NavBar = () => {
   const navOptions = (
@@ -17,13 +18,13 @@ const NavBar = () => {
 
   return (
     <>
-      <div className="navbar font-semibold fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white py-4">
+      <div className="navbar font-semibold fixed z-10 bg-opacity-30 max-w-screen-xl bg-black md:text-white md:py-4 py-2">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                className="h-5 w-5 text-white"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -43,13 +44,15 @@ const NavBar = () => {
               {navOptions}
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
+          <Link to="/" className="btn btn-ghost md:w-52 w-4/5">
+            <img src={logo} alt="" />
+           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{navOptions}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Get started</a>
+          <Link className="px-4 py-2 bg-slate-700 rounded text-white font-semibold duration-200 hover:bg-slate-950">Get started</Link>
         </div>
       </div>
     </>
